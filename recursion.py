43b1ceed6ae45633dfb0830e8eaf3f7b6a89fde9
James-Leslie <jlesl09@gmail.com>
2018-02-17 21:27:35 +0200

    Added docstrings

--- recursion.py
+++ recursion.py
@@ -1,14 +1,14 @@
 def sum_array(array):
-	
+
 	"""
 	Return sum of all items in array
 	"""
-	
-    if not array:
-        return 0
 
-    else:
-        return array[0] + sum_array(array[1:])
+	if not array:
+	    return 0
+
+	else:
+	    return array[0] + sum_array(array[1:])
 
 
 def fibonacci(n):
@@ -18,11 +18,11 @@ def fibonacci(n):
 	where nth term is equal to sum of previous two terms
 	"""
 
-    if n <= 1:
-        return n
+	if n <= 1:
+	    return n
 
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+	else:
+	    return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 def factorial(n):
@@ -31,11 +31,11 @@ def factorial(n):
 	Return n!
 	"""
 
-    if n == 0:
-        return 1
+	if n == 0:
+	    return 1
 
-    else:
-        return n * factorial(n - 1)
+	else:
+	    return n * factorial(n - 1)
 
 
 def reverse(word):
@@ -44,8 +44,8 @@ def reverse(word):
 	Return word in reverse
 	"""
 
-    if len(word) == 1:
-        return word
+	if len(word) == 1:
+	    return word
 
-    else:
-        return word[-1] + reverse(word[:-1])
+	else:
+	    return word[-1] + reverse(word[:-1])
